ce07bc58b4de4400b5fbb1c111bb2304c060275b
Reid Thompson <rpt265@uw.edu>
2015-10-07 16:56:03 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+Goal of JS code is to create a TicTacToe agent that never loses.
+The provided (original) agent selects its moves randomly.
+
---
+++ tictactoeAgent.js
@@ -0,0 +1,28 @@
+/* 
+ * Completed by Reid Thompson & Josh Rueschenberg
+ * For Dr. Chris Marriott
+ * TCSS 435 - Fall '15
+ * HW #1 - Tic Tac Toe
+ */
+
+var Agent = function () {
+	
+}
+
+//------------------------------------DIFFERENT AGENT ALGORITHMS---------------------------------------
+
+// OUR BEST SOLUTION...
+Agent.prototype.selectMove = function(board) {
+    
+}
+
+// ORIGINAL PROVIDED SOLUTION...
+/* Description: chooses a cell at random to play for each turn. */
+Agent.prototype.selectMoveAtRandom = function(board) {
+	var freeCells = [];
+    for (var i = 1; i < 10; i++) {
+        if (board.cellFree(i)) freeCells.push(i);
+    }
+
+    return freeCells[Math.floor(Math.random() * freeCells.length)];
+}
