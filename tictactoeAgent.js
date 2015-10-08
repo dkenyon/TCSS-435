8cfc7600e40b6bb56b243a645be4fb0b4e67b3dd
Reid Thompson <rpt265@uw.edu>
2015-10-08 13:41:39 -0700

    Broken code but starting point. Working on selectMoveWithRules() algorithm, currently developing helpers too.

--- tictactoeAgent.js
+++ tictactoeAgent.js
@@ -9,7 +9,7 @@ var Agent = function () {
 	
 }
 
-//------------------------------------DIFFERENT AGENT ALGORITHMS---------------------------------------
+//---------------HELPER METHODS---------------
 
 /*
  * Helper method - returns all free cells on gameboard.
@@ -21,6 +21,25 @@ Agent.prototype.getFreeCells = function(board) {
     }
     return freeCells;
 } 
+
+/*
+ * Helper method - returns a winning, if possible.
+ * Otherwise, if winning isn't an option, it returns -1.
+ */
+Agent.prototype.getWinningMove = function(freeCells) {
+	var returnedMoveIdx = -1; // assume you can't win unless a move is found
+	if (freeCells.length >= 8) { // neither player can win on first turn
+		return returnedMoveIdx;
+	} else {
+		for (var i = 0; i < freeCells.length; i++) {
+		var indvCell = freeCells[i];
+		// check if it's a corner
+
+	}
+	}
+}
+
+//---------------VARIOUS AGENT AI ALGORITHMS---------------
 
 // OUR BEST SOLUTION...LEAVE BLANK UNTIL ALL OPTIONS HAVE BEEN CONSIDERED
 // 
@@ -37,3 +56,16 @@ Agent.prototype.selectMoveAtRandom = function(board) {
 	var freeCells = getFreeCells(board);
     return freeCells[Math.floor(Math.random() * freeCells.length)];
 }
+
+/*
+ * Algorithm based on TicTacToe rules discussed in class.
+ */
+Agent.prototype.selectMoveWithRules = function(board) {
+	var freeCells = getFreeCells(board);
+
+	// Rule 1 - Win, if possible.
+	var winningMovesIdx = getWinningMove();
+	if (winningMovesIdx != -1) {
+		return winningMovesIdx;
+	}
+} 
