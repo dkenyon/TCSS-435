77e81437c366d4a01e8c80d6846a36c7a17bf038
Reid Thompson <rpt265@uw.edu>
2015-10-08 13:37:17 -0700

    Leave selectMove() blank until we've tested all algorithms for performance. This is tictactoeAgent.js's starting point before we develop individual algorithm methods and their helper methods.

--- tictactoeAgent.js
+++ tictactoeAgent.js
@@ -11,18 +11,29 @@ var Agent = function () {
 
 //------------------------------------DIFFERENT AGENT ALGORITHMS---------------------------------------
 
-// OUR BEST SOLUTION...LEAVE BLANK UNTIL ALL OPTIONS HAVE BEEN CONSIDERED
-Agent.prototype.selectMove = function(board) {
-	
-}
-
-// ORIGINAL PROVIDED SOLUTION...
-/* Description: chooses a cell at random to play for each turn. */
-Agent.prototype.selectMoveAtRandom = function(board) {
+/*
+ * Helper method - returns all free cells on gameboard.
+ */
+Agent.prototype.getFreeCells = function(board) {
 	var freeCells = [];
     for (var i = 1; i < 10; i++) {
         if (board.cellFree(i)) freeCells.push(i);
     }
+    return freeCells;
+} 
 
+// OUR BEST SOLUTION...LEAVE BLANK UNTIL ALL OPTIONS HAVE BEEN CONSIDERED
+// 
+Agent.prototype.selectMove = function(board) {
+	// Step 1: Figure out which cells are free to play on in gameboard
+	var freeCells = getFreeCells(board);
+}
+
+/* 
+ * Original provided solution.
+ * Description: chooses a cell at random to play for each turn. 
+ */
+Agent.prototype.selectMoveAtRandom = function(board) {
+	var freeCells = getFreeCells(board);
     return freeCells[Math.floor(Math.random() * freeCells.length)];
 }
