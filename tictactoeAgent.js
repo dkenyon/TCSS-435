64f3099ded20386b90d3e31aa37da319a1ad81e4
Josh <joshru@uw.edu>
2015-10-09 20:30:40 -0700

    Added initial moves function to all the code in other branch.

--- tictactoeAgent.js
+++ tictactoeAgent.js
@@ -39,6 +39,39 @@ var getCurrPlyr = function(board) {
 };
 
 /*
+ * Helper method - returns the current player's list of moves as an array of integers.
+ */
+var getCurrPlyrsPrevMoves = function(currPlyr, board) {
+    var currPlyrsPrevMoves = [];
+    if (currPlyr === 1) { // X's move
+        currPlyrsPrevMoves = board.X;
+    } else { // currPlyr == 2 (O's move)
+        currPlyrsPrevMoves = board.O;
+    }
+
+    return currPlyrsPrevMoves;
+};
+
+/*
+ * Helper method - returns true if given move choice creates a threeInARow.
+ * Returns false otherwise.
+ */
+var determineWhetherMoveWinsGame = function(currPlyrsPrevMoves, firstCell, secondCell) {
+    var controlsFirstCell= false;
+    var controlsSecondCell = false;
+    for (var i = 0; i < currPlyrsPrevMoves.length; i++) {
+        var currMove = currPlyrsPrevMoves[i];
+        if (currMove === firstCell) {
+            controlsFirstCell = true;
+        } else if (currMove === secondCell) {
+            controlsSecondCell= true;
+        }
+    }
+    return controlsFirstCell && controlsSecondCell;
+};
+
+
+/*
  * When less than 4 moves have been made total (2 by each player),
  * the next move can be selected pseudo-randomly with some guidance
  */
@@ -66,9 +99,286 @@ var getInitialMoves = function(freeCells, currPlyr, board) {
 };
 
 /*
- * Helper method - returns a winning, if possible.
+ * Helper method - returns true if indvCell (corner cell) is a winning move and false otherwise.
+ */
+var winningCornerMoveCheck = function(indvCell, currPlyr, board) {
+    var isWinningMove = false;
+
+    var currPlyrsPrevMoves = getCurrPlyrsPrevMoves(currPlyr, board);
+
+    var threeInARowFromCornerMove = false; // assume game's not won yet
+    if (indvCell === 8) { // top left
+        threeInARowFromCornerMove = determineWhetherMoveWinsGame(currPlyrsPrevMoves, 1, 6) // check right
+    || determineWhetherMoveWinsGame(currPlyrsPrevMoves, 3, 4); // check down
+    } else if (indvCell === 6) { // top right
+        threeInARowFromCornerMove = determineWhetherMoveWinsGame(currPlyrsPrevMoves, 1, 8) // check left
+            || determineWhetherMoveWinsGame(currPlyrsPrevMoves, 7, 2); // check down
+    } else if (indvCell === 4) { // bottom left
+        threeInARowFromCornerMove = determineWhetherMoveWinsGame(currPlyrsPrevMoves, 9, 2) // check right
+            || determineWhetherMoveWinsGame(currPlyrsPrevMoves, 3, 8); // check up
+    } else { // indvCell === 2 (bottom right)
+        threeInARowFromCornerMove = determineWhetherMoveWinsGame(currPlyrsPrevMoves, 9, 4) // check left
+            || determineWhetherMoveWinsGame(currPlyrsPrevMoves, 7, 6); // check up
+    }
+
+    return threeInARowFromCornerMove;
+};
+
+/*
+ * Helper method - returns true if indvCell (side middle) is a winning move and false otherwise.
+ */
+var winningSideMiddleMoveCheck = function(indvCell, currPlyr, board) {
+    var isWinningMove = false;
+
+    var currPlyrsPrevMoves = getCurrPlyrsPrevMoves(currPlyr, board);
+
+    var threeInARowFromMiddleMove = false; // assume game's not won yet
+
+    if (indvCell === 1) { // top center
+        threeInARowFromMiddleMove = determineWhetherMoveWinsGame(currPlyrsPrevMoves, 8, 6) // check horizontal
+    || determineWhetherMoveWinsGame(currPlyrsPrevMoves, 5, 9); // check vertical
+    } else if (indvCell === 7) { // right center
+        threeInARowFromMiddleMove = determineWhetherMoveWinsGame(currPlyrsPrevMoves, 3, 5) // check horizontal
+            || determineWhetherMoveWinsGame(currPlyrsPrevMoves, 6, 2); // check vertical
+    } else if (indvCell === 9) { // bottom center
+        threeInARowFromMiddleMove = determineWhetherMoveWinsGame(currPlyrsPrevMoves, 4, 2) // check horizontal
+            || determineWhetherMoveWinsGame(currPlyrsPrevMoves, 1, 5); // check vertical
+    } else { // indvCell === 3 (left center)
+        threeInARowFromMiddleMove = determineWhetherMoveWinsGame(currPlyrsPrevMoves, 5, 7) // check horizontal
+            || determineWhetherMoveWinsGame(currPlyrsPrevMoves, 8, 4); // check vertical
+    }
+
+    return threeInARowFromMiddleMove;
+};
+
+/*
+ * Helper method - returns true if indvCell (center) is a winning move and false otherwise.
+ */
+var winningCenterMoveCheck = function(indvCell, currPlyr, board) {
+    var isWinningMove = false;
+
+    var currPlyrsPrevMoves = getCurrPlyrsPrevMoves(currPlyr, board);
+
+    return determineWhetherMoveWinsGame(currPlyrsPrevMoves, 1, 9) // checks vertical
+        || determineWhetherMoveWinsGame(currPlyrsPrevMoves, 3, 7) // checks horizontal
+        || determineWhetherMoveWinsGame(currPlyrsPrevMoves, 8, 2) // checks top left to bottom right
+        || determineWhetherMoveWinsGame(currPlyrsPrevMoves, 6, 4); // checks top right to bottom left
+};
+
+/*
+ * Helper method - returns a winning move's index, if possible.
  * Otherwise, if winning isn't an option, it returns -1.
  */
+var getWinningMove = function(freeCells, currPlyr, board) {
+    var returnedMoveIdx = -1; // assume you can't win unless a move is found
+    for (var i = 0; i < freeCells.length; i++) {
+        var indvCell = freeCells[i];
+        // check if winning move could be a corner cell
+        if (indvCell === 8 || indvCell === 6 || indvCell === 4 || indvCell === 2) {
+            var cornerMoveWins = winningCornerMoveCheck(indvCell, currPlyr, board);
+            if (cornerMoveWins) {
+                returnedMoveIdx = indvCell;
+            }
+            // check if winning move could be a 'side middle' cell
+        } else if (indvCell === 1 || indvCell === 3 || indvCell === 7 || indvCell === 9) {
+            var sideMiddleMoveWins = winningSideMiddleMoveCheck(indvCell, currPlyr, board);
+            if (sideMiddleMoveWins) {
+                returnedMoveIdx = indvCell;
+            }
+            // check if winning move could be center cell
+        } else { // indvCell === 5
+            var centerMoveWins = winningCenterMoveCheck(indvCell, currPlyr, board);
+            if (centerMoveWins) {
+                returnedMoveIdx = indvCell;
+            }
+        }
+    }
+    return returnedMoveIdx;
+};
+
+/*
+ * Helper method - returns true if opposing player has a twoInARow that needs blocking and false otherwise.
+ */
+var determineWhetherOpponentNeedsToBeBlocked = function(freeCells, currPlyrsPrevMoves, oppPlyrsPrevMoves) {
+    var oppNeedsToBeBlocked = false; // assume that currPlyr doesn't need to block opponent unless necessary
+
+
+};
+
+/*
+ * Helper method - returns a blocking move's index, if necessary (meaning opponent has a twoInARow).
+ * Otherwise, it returns -1.
+ */
+var getBlockingMove = function(freeCells, currPlyrsIdx, oppPlyrsIdx, board)  { // all params necessary?
+    var blockingMovesIdx = -1; // assume not blocking move is necessary unless one is found
+
+    var currPlyrsPrevMoves = getCurrPlyrsPrevMoves(currPlyrsIdx);
+    var oppPlyrsPrevMoves = getCurrPlyrsPrevMoves(oppPlyrsIdx);
+
+    var needToBlockOpponent = determineWhetherOpponentNeedsToBeBlocked(freeCells, currPlyrsPrevMoves, oppPlyrsPrevMoves);
+    if (needToBlockOpponent) {
+        blockingMovesIdx = getBlockingMove()
+    }
+};
+
+//---------------VARIOUS AGENT AI ALGORITHMS---------------
+
+// OUR BEST SOLUTION...LEAVE BLANK UNTIL ALL OPTIONS HAVE BEEN CONSIDERED
+// Reid: I think steps 1 & 2 are necessary.
+Agent.prototype.selectMove = function(board) {
+    // Step 1: Figure out which cells are free to play on in gameboard
+    var freeCells = getFreeCells(board);
+
+    // Step 2: Determine which player's turn it is
+    var currPlyr = getCurrPlyr(board);
+
+    return selectMoveWithRules(board);
+};
+
+/*
+ * Original provided solution.
+ * Description: chooses a cell at random to play for each turn.
+ */
+Agent.prototype.selectMoveAtRandom = function(board) {
+    var freeCells = getFreeCells(board);
+    return freeCells[Math.floor(Math.random() * freeCells.length)];
+
+};
+
+/*
+ * Algorithm based on TicTacToe rules discussed in class and from the TicTacToe Wikipedia article.
+ * That article has be found here: https://en.wikipedia.org/wiki/Tic-tac-toe#Strategy .
+ */
+var selectMoveWithRules = function(board) {
+    var optimalMovesIdx = -1;
+
+    // get all free cells on gameboard
+    var freeCells = getFreeCells(board);
+
+    // determine which player's turn it is
+    var currPlyr = getCurrPlyr(board);
+
+    // START OF RULE-BASED ALGORITHM!
+
+    // TODO: ***still need to handle edge cases (ie. 1st, 2nd, 3rd, & 4th moves)***
+
+    // Rule 0 - handle edge cases
+    // ...
+    if (freeCells.length >= 6) {
+        return getInitialMoves(freeCells, currPlyr, board);
+    }
+
+    // Rule 1 - Win, if possible.
+    var winningMovesIdx;
+    if (freeCells.length <= 5) { // then it's possible to have a winning move
+        winningMovesIdx = getWinningMove(freeCells, currPlyr, board);
+    }
+    if (winningMovesIdx != -1) {
+        optimalMovesIdx = winningMovesIdx;
+    }
+
+    // Rule 2 - Block opponent if they have a twoInARow.
+    var blockingMovesIdx;
+    var oppPlyrsIdx = currPlyr == 1 ? 2 : 1;
+    if (freeCells.length <= 5) {
+        // all params in this function call necessary? - ie. board & freeCells
+        blockingMovesIdx = getBlockingMove(freeCells, currPlyrsIdx, oppPlyrsIdx, board);
+    }
+    if (blockingMovesIdx != -1) {
+        optimalMovesIdx = blockingMovesIdx;
+    }
+
+    // Rule 3 - ...
+
+    return optimalMovesIdx;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//---------------HELPER METHODS---------------
+
+/!*
+ * Helper method - returns all free cells on gameboard.
+ *!/
+var getFreeCells = function(board) {
+    var freeCells = [];
+    for (var i = 1; i < 10; i++) {
+        if (board.cellFree(i)) freeCells.push(i);
+    }
+    return freeCells;
+};
+
+/!*
+ * Helper method - determines which player's turn it is currently.
+ * Returns a 1 for P1 (Xs) and a 2 for P2 (Os).
+ *!/
+var getCurrPlyr = function(board) {
+    var p1Moves = board.X; // remember! - X's always go first
+    var p2Moves = board.O;
+    var currPlyrsIdx = -1; // initial invalid value (for error handling)
+    if (p1Moves.length > p2Moves.length) {
+        currPlyrsIdx = 2; // O's turn
+    } else { // p1Moves.length == p2Moves.length
+        currPlyrsIdx = 1; // X's turn'
+    }
+    return currPlyrsIdx;
+};
+
+/!*
+ * When less than 4 moves have been made total (2 by each player),
+ * the next move can be selected pseudo-randomly with some guidance
+ *!/
+var getInitialMoves = function(freeCells, currPlyr, board) {
+    var p1Moves = board.X;
+    var p2Moves = board.O;
+    //random number between 1 and 4 to choose a starting corner if we're player 1
+    var randCorner = Math.floor(Math.random() * 4) + 1;
+
+    if (p1Moves.length <= 2 || p2Moves.length <= 2) {
+        if (currPlyr === 2 && p1Moves.indexOf(5) < 0) {
+            return 5;
+        } else {
+            if (randCorner === 1 && freeCells.indexOf(8) >= 0) {
+                return 8;
+            } else if (randCorner === 2 && freeCells.indexOf(6) >= 0) {
+                return 6;
+            } else if (randCorner === 3 && freeCells.indexOf(4) >= 0) {
+                return 4;
+            } else {
+                return 2;
+            }
+        }
+    }
+};
+
+/!*
+ * Helper method - returns a winning, if possible.
+ * Otherwise, if winning isn't an option, it returns -1.
+ *!/
 var getWinningMove = function(freeCells, currPlyr, board) {
     var returnedMoveIdx = -1; // assume you can't win unless a move is found
     for (var i = 0; i < freeCells.length; i++) {
@@ -83,9 +393,9 @@ var getWinningMove = function(freeCells, currPlyr, board) {
     }
 };
 
-/*
+/!*
  * Helper method - returns true if indvCell is a winning move and false otherwise.
- */
+ *!/
 var winningCornerMoveCheck = function(indvCell, currPlyr, board) {
     var isWinningMove = false;
     if (indvCell == 8) { // top left
@@ -132,18 +442,18 @@ Agent.prototype.selectMove = function(board) {
 
 };
 
-/*
+/!*
  * Original provided solution.
  * Description: chooses a cell at random to play for each turn.
- */
+ *!/
 Agent.prototype.selectMoveAtRandom = function(board) {
     var freeCells = getFreeCells(board);
     return freeCells[Math.floor(Math.random() * freeCells.length)];
 };
 
-/*
+/!*
  * Algorithm based on TicTacToe rules discussed in class.
- */
+ *!/
 Agent.prototype.selectMoveWithRules = function(board) {
     // get all free cells on gameboard
     var freeCells = getFreeCells(board);
@@ -162,133 +472,9 @@ Agent.prototype.selectMoveWithRules = function(board) {
 
     // Rule 2 - ...
 };
+*/
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///**
-// * Completed by Reid Thompson & Josh Rueschenberg
-// * For Dr. Chris Marriott
-// * TCSS 435 - Fall '15
-// * HW #1 - Tic Tac Toe
-// */
-//
-//
-//var Agent = function () {
-//
-//};
-//
-////------------------------------------DIFFERENT AGENT ALGORITHMS---------------------------------------
-//
-//// OUR BEST SOLUTION...
-//Agent.prototype.selectMove = function(board) {
-//	// get all free cells
-//    var freeCells = getFreeCells(board);
-//
-//    // get previous move indices for X and O
-//    var xMoves = board.X;
-//    var oMoves = board.O;
-//
-//    if (xMoves.length === 0 && oMoves.length === 0) {
-//        return 5;
-//    }
-//
-//    //return null;
-//    //if (freeCells.indexOf(5) > -1) {
-//    //    return pickCenter();
-//    //} else {
-//    //    return pickCorner(freeCells);
-//    //}
-//
-//};
-//
-///*
-//
-//// ORIGINAL PROVIDED SOLUTION...
-///!** Description: chooses a cell at random to play for each turn. *!/
-//Agent.prototype.selectMoveAtRandom = function(board) {
-//	var freeCells = [];
-//    for (var i = 1; i < 10; i++) {
-//        if (board.cellFree(i)) freeCells.push(i);
-//    }
-//
-//    return freeCells[Math.floor(Math.random() * freeCells.length)];
-//};
-//
-//*/
-//
-//var getFreeCells = function(board) {
-//    var freeCells = [];
-//    for (var i = 1; i < 10; i++) {
-//        if (board.cellFree(i)) freeCells.push(i);
-//    }
-//    return freeCells;
-//};
-//
-//var getXMoves = function(board) {
-//    return board.X;
-//};
-//
-//var getOMoves = function(board) {
-//    return board.O;
-//};
-//
-//var letsWin = function(board) {
-//
-//};
-//
-//var pickCenter = function() {
-//  return 5;
-//};
-//
-//var pickCorner = function(freeCells) {
-//    var result = 0;
-//    if (freeCells.indexOf(8) > -1) {
-//        result = 8;
-//    } else if (freeCells.indexOf(6) > -1) {
-//        result = 6;
-//    } else if (freeCells.indexOf(4) > -1) {
-//        result = 4;
-//    } else if (freeCells.indexOf(2) > -1) {
-//        result = 2;
-//    }
-//    return result;
-//};
-//
-//var blockMove = function(board, enemyMoves) {
-//    //start with invalid index, implying no move to block
-//    result = -1;
-//
-//};
 
